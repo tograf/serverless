bfa0ea5015daa308030b13cba38d9e322df95d53
Tobias Graf <tobias.graf@gmx.net>
2021-03-28 15:21:15 +0200

    new client id

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'jlg3ljpba2'
+const apiId = 'egqcvvgo76'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
